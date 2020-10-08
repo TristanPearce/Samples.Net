4729a9c135de24f148878f1df2608eabda9d282b
Tristan Pearce <tristanpearce24@gmail.com>
2020-10-08 01:35:28 +0100

    Create ServiceProvider class and IService instance.

---
+++ Samples.Net/ServiceProvider/ServiceProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.Net
+{
+    internal sealed class ServiceProvider : IServiceProvider
+    {
+
+        private IDictionary<Type, IService> services; 
+
+        public ServiceProvider()
+        {
+            services = new Dictionary<Type, IService>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if(services.TryGetValue(serviceType, out IService service))
+            {
+                return service.Get();
+            }
+
+            return null;
+        }
+    }
+}
