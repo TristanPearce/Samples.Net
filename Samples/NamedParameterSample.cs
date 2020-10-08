85675b6150223f888bec2212883baa06814adaee
Tristan Pearce <tristanpearce24@gmail.com>
2020-10-08 02:35:19 +0100

    Add sample for NamedParameter.

---
+++ Samples/NamedParameterSample.cs
@@ -0,0 +1,22 @@
+﻿using Samples.Net;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples
+{
+    [Sample]
+    public class NamedParameterSample
+    {
+
+
+        [Run]
+        public void Login(string token)
+        {
+            Console.WriteLine($"Login token: {token}");
+        }
+
+
+    }
+}
