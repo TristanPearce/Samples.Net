cb8da685fb55e509c4a69635bd3f2098125a494c
Tristan Pearce <tristanpearce24@gmail.com>
2020-10-08 01:38:45 +0100

    Create SingletonService.

---
+++ Samples.Net/ServiceProvider/SingletonService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.Net.ServiceProvider
+{
+    internal class SingletonService : IService
+    {
+        private readonly object instance;
+
+        public SingletonService(object instance)
+        {
+            this.instance = instance;                        
+        }
+
+        public object Get()
+        {
+            return instance;
+        }
+    }
+}
